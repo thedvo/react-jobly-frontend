890e9792ac9fb483d3b886bcb165d27c6d82ebcf
Dan Vo <danvo.vo@gmail.com>
2022-07-17 12:24:53 -0700

    deploy surge app and edit README

---
+++ build/precache-manifest.538e3204acbd40898d60a5639b387c5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "79c5a5a9d47e4a84d349",
+    "url": "/static/css/main.e3065420.chunk.css"
+  },
+  {
+    "revision": "79c5a5a9d47e4a84d349",
+    "url": "/static/js/main.79c5a5a9.chunk.js"
+  },
+  {
+    "revision": "75565d231d7652571999",
+    "url": "/static/css/1.a8f1caa1.chunk.css"
+  },
+  {
+    "revision": "75565d231d7652571999",
+    "url": "/static/js/1.75565d23.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cd4e71338af151f4a2a6438155e248d5",
+    "url": "/index.html"
+  }
+];
